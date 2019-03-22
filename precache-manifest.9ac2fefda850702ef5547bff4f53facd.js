9ca471d3656236667f11ecad2c0df5a84a703b47
madoke <madoke@gmail.com>
2019-03-22 21:28:26 +0000

    Updates

---
+++ precache-manifest.9ac2fefda850702ef5547bff4f53facd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f507865a80f107e4ada82c5e74a870f",
+    "url": "/random-eats/static/media/italian.4f507865.jpg"
+  },
+  {
+    "revision": "9da764245261b7219c08",
+    "url": "/random-eats/static/css/main.e92bb843.chunk.css"
+  },
+  {
+    "revision": "249ff7d89f9925f6cbe8",
+    "url": "/random-eats/static/js/runtime~main.a913b46c.js"
+  },
+  {
+    "revision": "4df5864eb04e7f22629a",
+    "url": "/random-eats/static/js/2.cb6c132e.chunk.js"
+  },
+  {
+    "revision": "06d95b88d49b52df4a04e5b8fca4c25e",
+    "url": "/random-eats/static/media/mexican.06d95b88.jpg"
+  },
+  {
+    "revision": "5a156a5108bfe8277518f4b823c332b2",
+    "url": "/random-eats/static/media/japanese.5a156a51.png"
+  },
+  {
+    "revision": "bf545a282d1bac300af5d5e87cd1840e",
+    "url": "/random-eats/static/media/indian.bf545a28.jpg"
+  },
+  {
+    "revision": "9da764245261b7219c08",
+    "url": "/random-eats/static/js/main.e65a7986.chunk.js"
+  },
+  {
+    "revision": "da9e71f5fc1d0bf4ff92a11e5e2be3dd",
+    "url": "/random-eats/static/media/blockhouse.da9e71f5.jpg"
+  },
+  {
+    "revision": "0d0c4af048b832f305a8a0c7e4319b07",
+    "url": "/random-eats/static/media/indian.0d0c4af0.mp3"
+  },
+  {
+    "revision": "0566c76522721d49a03dd0521e4a7d95",
+    "url": "/random-eats/static/media/japanese.0566c765.mp3"
+  },
+  {
+    "revision": "2c08b94ea04838cc68599453d81e1cdd",
+    "url": "/random-eats/static/media/italian.2c08b94e.mp3"
+  },
+  {
+    "revision": "b7d76af46bb0fcac896e3246d724cce5",
+    "url": "/random-eats/static/media/mexican.b7d76af4.mp3"
+  },
+  {
+    "revision": "f2afec55902ad918d8087d8c281ebf05",
+    "url": "/random-eats/static/media/blockhouse.f2afec55.mp3"
+  },
+  {
+    "revision": "1391ca71a50ca501b2b6ce74fdd99bb1",
+    "url": "/random-eats/index.html"
+  }
+];
